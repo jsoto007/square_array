82373efb108c38147bd086ed4d0fd00c4b4ae4a5
Jose Soto <jsoto7087@gmail.com>
2021-01-19 16:41:14 -0500

    commit tree 4b81bfdbf93e4055d5242e3ac5ed7b598c2af9c0

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
+#def square_array(array)
+ # array = [1, 2, 3, 4, 5]
+  #array.each {|square| puts "#{square}"}
+
   # your code here
-end
+#end
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end 
